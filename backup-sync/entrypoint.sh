2c620d1136d67f26668dc022a08482057e41edba
Vladislav Tankov <vdtankov@gmail.com>
2017-09-04 11:03:52 +0300

    Add resync option in S3 Buckets Sync task.

--- backup-sync/entrypoint.sh
+++ backup-sync/entrypoint.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-aws s3 sync  "s3://${BACKUP_PROD}" "s3://${BACKUP_STAGING}"
+if [[ ${RESYNC} == 0 ]]; then
+    aws s3 sync  "s3://${BACKUP_PROD}" "s3://${BACKUP_STAGING}"
+else
+    aws s3 sync  "s3://${BACKUP_PROD}" "s3://${BACKUP_STAGING}" --delete
+fi
