0789c05a6d83c7bd243fb2c50fa7ea7c1be18ced
Vladislav Tankov <vdtankov@gmail.com>
2018-06-26 13:00:06 +0300

    Update backup task to support multi tables

--- backup/entrypoint.sh
+++ backup/entrypoint.sh
@@ -4,20 +4,31 @@ function upload_dir {
     FROM=$1
     TO=$2
     echo "Uploading dir from $FROM to $TO"
-    aws s3 sync "${FROM}" "s3://${TO}" --quiet --delete
+    aws s3 sync "${FROM}" "s3://${TO}" --quiet
+}
+
+function clear_dir {
+    PATH=$1
+    echo "Clearing dir s3://${PATH}"
+    aws s3 rm --recursive "s3://${PATH}"
 }
 
 function backup_partition {
-    TABLE=$1
+    CUR_TABLES=$1
     PARTITION=$2
     KEY=$3
 
-    echo "Backuping table ${TABLE}"
-    echo "SQL: ALTER TABLE ${TABLE} FREEZE PARTITION '${PARTITION}'"
-    echo "ALTER TABLE ${TABLE} FREEZE PARTITION '${PARTITION}'" | POST "http://${USERNAME}:${PASSWORD}@localhost:8123/"
+    clear_dir "${S3_BACKUPS_BUCKET}/${PARTITION}/${KEY}"
 
-    CURRENT_NUMBER=$(cat ${BACKUP_PATH}/increment.txt)
-    upload_dir "${BACKUP_PATH}/${CURRENT_NUMBER}" "${S3_BACKUPS_BUCKET}/${PARTITION}/${KEY}"
+    read -r -a array <<< "$CUR_TABLES"
+    for TABLE in "${array[@]}"; do
+        echo "Backuping table ${TABLE}"
+        echo "SQL: ALTER TABLE ${TABLE} FREEZE PARTITION '${PARTITION}'"
+        echo "ALTER TABLE ${TABLE} FREEZE PARTITION '${PARTITION}'" | POST "http://${USERNAME}:${PASSWORD}@localhost:8123/"
+
+        CURRENT_NUMBER=$(cat ${BACKUP_PATH}/increment.txt)
+        upload_dir "${BACKUP_PATH}/${CURRENT_NUMBER}" "${S3_BACKUPS_BUCKET}/${PARTITION}/${KEY}"
+    done
 }
 
 function clear_backup_dir {
@@ -32,27 +43,18 @@ function main {
     echo "Current date: ${DAY}/${MONTH}/${YEAR}"
 
     if [[ ${FULL_BACKUP} == 0 ]]; then
-        FS=', ' read -r -a array <<< "$TABLES"
-        for TABLE in "${array[@]}"; do
-            if [[ ${DAY} = "${DAY_TO_UPDATE_LAST_MONTH}" ]]; then
-                backup_partition ${TABLE} $(date -d "-1 month" +"%Y%m") "full"
-            fi
-            backup_partition ${TABLE} $(date +"%Y%m") ${DAY}
-        done
+        if [[ ${DAY} = "${DAY_TO_UPDATE_LAST_MONTH}" ]]; then
+            backup_partition ${TABLES} $(date -d "-1 month" +"%Y%m") "full"
+        fi
+        backup_partition ${TABLES} $(date +"%Y%m") ${DAY}
     else
         for CUR_YEAR in $(seq ${START_YEAR} ${YEAR}); do
             for CUR_MONTH in 01 02 03 04 05 06 07 08 09 10 11 12; do
                 if [[ ${CUR_YEAR} =  ${YEAR} && ${CUR_MONTH} = ${MONTH} ]]; then
-                    FS=', ' read -r -a array <<< "$TABLES"
-                    for TABLE in "${array[@]}"; do
-                        backup_partition ${TABLE} "${CUR_YEAR}${CUR_MONTH}" ${DAY}
-                    done
+                    backup_partition ${TABLES} "${CUR_YEAR}${CUR_MONTH}" ${DAY}
                     break
                 fi
-                FS=', ' read -r -a array <<< "$TABLES"
-                for TABLE in "${array[@]}"; do
-                    backup_partition ${TABLE} "${CUR_YEAR}${CUR_MONTH}" "full"
-                done
+                backup_partition ${TABLES} "${CUR_YEAR}${CUR_MONTH}" "full"
             done
         done
     fi
